769dba8ae6603e578e478d781ccd787f626c4dda
UmarEngineer <ohojimirzayev@gmail.com>
2022-08-17 16:51:15 +0500

    First Commit

---
+++ src/language.js
@@ -0,0 +1,37 @@
+const uz = {
+  settings: 'Tweet Sozlamalari',
+  name: 'Ism Familya',
+  username: 'Foydalanuvchi nomi',
+  quotes: 'Iqtiboslar',
+  likes: 'Yoqdi',
+  verified: 'Tasdiqlangan',
+  select: ['Ha', "Yo'q"],
+  download: 'Tweetni yuklab olish',
+  create: 'Hosil qilish',
+  writeUsername: 'Twitter foydalanuvchini nomingizni yozing',
+  getInfo: "Ma'lumotlarni olish",
+  exampleTweet: 'Bu yerda test uchun hosil qilingan tweet bo\'ladi'
+};
+
+const en = {
+  settings: 'Tweet Settings',
+  name: 'Name and Surname',
+  username: 'Username',
+  quotes: 'Quote Tweets',
+  likes: 'Likes',
+  verified: 'Verified',
+  select: ['Yes', 'No'],
+  download: 'Download Tweet',
+  create: 'Create Tweet',
+  writeUsername: 'Write your twitter username',
+  getInfo: "Get user info",
+  exampleTweet: 'Here be example tweet'
+
+};
+
+const language = {
+  uz,
+  en
+};
+
+export { language };
